3d13376875163c6ae49d9eaabc781744e48e7428
angelxwars <angelxwars@gmail.com>
2020-12-02 21:04:18 +0100

    fichero de ejecucion en windows cambiado

--- start_hacid_docker.bat
+++ start_hacid_docker.bat
@@ -2,6 +2,6 @@ docker stop hbase-docker
 docker rm hbase-docker
 docker image rm hbase-hacid:1.0
 
-docker build -t hbase-hacid:1.0 .
+docker build -t angelxwars/hbase-hacid:1.0 .
 
-docker run --name=hbase-docker -P -it -h hbase-docker -d -v "G:\josea\Desktop\data-angel\input":/input -v "G:\josea\Desktop\data-angel\output":/output hbase-hacid:1.0
+docker run --name=hbase-docker -P -it -h hbase-docker -d -v "G:\josea\Desktop\data-angel\input":/input -v "G:\josea\Desktop\data-angel\output":/output angelxwars/hbase-hacid:1.0
